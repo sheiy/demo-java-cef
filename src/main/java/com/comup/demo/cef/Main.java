408730e9c46e342ac8634352868896eff8c0508d
Comup <35429674@qq.com>
2017-07-12 20:57:23 +0800

    添加flash支持

--- src/main/java/com/comup/demo/cef/Main.java
+++ src/main/java/com/comup/demo/cef/Main.java
@@ -12,11 +12,18 @@ package com.comup.demo.cef;/*
 
 import org.cef.CefApp;
 import org.cef.CefClient;
+import org.cef.CefSettings;
+import org.cef.OS;
 import org.cef.browser.CefBrowser;
 import org.cef.browser.CefBrowserFactory;
 import org.cef.browser.CefRequestContext;
+import org.cef.callback.CefCommandLine;
+import org.cef.handler.CefAppHandlerAdapter;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.net.URL;
 
 /**
  * <一句话功能简介><br>
@@ -30,12 +37,32 @@ import java.awt.*;
  */
 public class Main {
     public static void main(String[] args) {
-        Frame frame = new Frame();
-        frame.setSize(800,600);
-        CefClient cefClient = CefApp.getInstance().createClient();
-        CefBrowser cefBrowser = CefBrowserFactory.create(cefClient, "http://www.baidu.com/", false, false, CefRequestContext.getGlobalContext());
-        Component uiComponent = cefBrowser.getUIComponent();
+        final Frame frame = new Frame();
+        frame.setSize(800, 600);
+        CefSettings settings = new CefSettings();
+        settings.windowless_rendering_enabled= OS.isLinux();
+        settings.background_color = settings.new ColorType(100, 255, 242, 211);
+        settings.locale = "zh-CN";
+        CefApp cefApp = CefApp.getInstance(args, settings);
+        CefApp.addAppHandler(new CefAppHandlerAdapter(args) {
+            @Override
+            public void onBeforeCommandLineProcessing(String process_type, CefCommandLine command_line) {
+                super.onBeforeCommandLineProcessing(process_type, command_line);
+                //启动flash支持需要系统安装flash
+                command_line.appendSwitch("--enable-system-flash");
+            }
+        });
+        CefClient cefClient = cefApp.createClient();
+        CefBrowser browser = cefClient.createBrowser("http://www.baidu.com/", OS.isLinux(), false);
+        Component uiComponent = browser.getUIComponent();
         frame.add(uiComponent);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                CefApp.getInstance().dispose();
+                frame.dispose();
+            }
+        });
         frame.setVisible(true);
     }
 }
